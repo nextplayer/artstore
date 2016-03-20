2894ac11ee18d347c14c43c1997f14ed36c1755d
yenshiang <nextplayer@gmail.com>
2016-03-20 22:12:38 +0800

    week2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,6 +239,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, '1054041934639294', '039d5fb65b916bf425e6e3d4b1f43d74', scope: 'email, publish_actions'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,31 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   #root "welcome#index"
   root "products#index"
 
+  resources :orders do
+    member do
+      get :pay_with_credit_card
+    end
+  end
+
+  resources :carts do
+    collection do
+      post :checkout
+      delete :clean
+    end
+  end
+
   namespace :admin do
     resources :products
+    resources :orders do
+       member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :users do
       member do
         post :to_admin
@@ -13,7 +34,17 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+
+  namespace :account do
+    resources :orders
+  end
+
+  resources :items, controller: "cart_items"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
